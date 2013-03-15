efe4dcfabcdf087b82257f4d585778e61c190af9
Maurice Wahba <wahbinatorm@gmail.com>
2013-03-15 21:20:41 +0400

    API does shows, streams don't show admin user/pass

--- api.php
+++ api.php
@@ -20,15 +20,37 @@
 	if(php_sapi_name() != "cli") {
 		exit("This file can only be ran as a server script.");
 	}
+
+	if($argc < 2) {
+		exit("\nUSAGE:\napi.php [command]\n\nCOMMANDS:\nstreams\t\tDisplay list of streams\nschedule\tDisplay schedule\n\n");
+	}
+	
+	$cmd = $argv[1];
 	
 	$db = new Database(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-	
-	$result = $db->getTable("streams");
 	$output = array();
 	
-	while($row = $result->fetch_assoc()) {
-		$output[] = $row;
+	switch($cmd) {
+		case "streams":
+			$result = $db->getTable("streams");
+			while($row = $result->fetch_assoc()) {
+				unset($row['username']);
+				unset($row['password']);
+				$output[] = $row;
+			}
+			break;
+		case "schedule":
+			$result = $db->getTable("shows");
+			while($row = $result->fetch_assoc()) {
+				$output[] = $row;
+			}
+			break;
+		default:
+			exit("Invalid command specified.\n");
+			break;
 	}
+
+	
 	
 	header("Content-type: application/json");
 	//print json_encode(array("message"=>"This is only a test. Passwords will be removed in the final product."));
